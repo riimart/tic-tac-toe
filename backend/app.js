a85ca5dbcdb07683b0c6a20208e90cd138b5c900
rey-mitacor <150757439+rey-mitacor@users.noreply.github.com>
2024-09-18 02:44:31 +0800

    Fix Links

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/matches", matchesRoutes);
+app.use("https://tic-tac-toe-gq68.onrender.com/api/matches", matchesRoutes);
 
 // connect to db
 mongoose
